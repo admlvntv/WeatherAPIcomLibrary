875fd4866dd14c52165d73abcd15bdb97a1e3710
Adam Nativ <admlvntv@gmail.com>
2022-01-26 17:49:36 -0500

    Add getCurrentFeelsLikeTemp, getUVIndex, getWindSpeed, and checkIsDay

--- WeatherAPIcomLib/src/main/java/me/adamcraftmaster/CurrentWeatherLib.java
+++ WeatherAPIcomLib/src/main/java/me/adamcraftmaster/CurrentWeatherLib.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import me.adamcraftmaster.schema.currentweather.*;
-import me.adamcraftmaster.utils.JSONParser;
+import me.adamcraftmaster.utils.JSONParserUtil;
 
 public class CurrentWeatherLib {
     private final String apiKey;
@@ -26,7 +26,7 @@ public class CurrentWeatherLib {
      */
     private CurrentWeather currentInfoDataSource(String region) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(JSONParser.urlToJson("https://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + region + "&aqi=no"), CurrentWeather.class);
+        return objectMapper.readValue(JSONParserUtil.urlToJson("https://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + region + "&aqi=no"), CurrentWeather.class);
     }
     
     /** 
@@ -62,5 +62,69 @@ public class CurrentWeatherLib {
                 //defaults to celcius if no valid tempScale given
                 return currentInfoDataSource(region).getCurrent().getTempC();
         }
+    }
+
+    /** 
+     * Gets the current feels like temperature from a given region as text
+     * @param region the region, can be given as US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name
+     * @param tempScale the scale of temperature, send 'F','f','C', or 'c' defaults to celcius if invalid tempScale given
+     * @return temperature in either farenheight or celcius, depending on tempScale
+     * @throws JsonProcessingException
+     */
+    public double getCurrentFeelsLikeTemp(String region, char tempScale) throws JsonProcessingException {
+        switch(tempScale) {
+            case 'F':
+                return currentInfoDataSource(region).getCurrent().getFeelslikeF();
+            case 'f':
+                return currentInfoDataSource(region).getCurrent().getFeelslikeF();
+            case 'C':
+                return currentInfoDataSource(region).getCurrent().getFeelslikeC();
+            case 'c':
+                return currentInfoDataSource(region).getCurrent().getFeelslikeC();
+            default:
+                //defaults to celcius if no valid tempScale given
+                return currentInfoDataSource(region).getCurrent().getFeelslikeC();
+                
+        }
+    }
+
+    /** 
+     * Gets the current UV index.
+     * @param region the region, can be given as US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name
+     * @return uv index as a double
+     * @throws JsonProcessingException
+     */
+    public double getUVIndex(String region) throws JsonProcessingException {
+        return currentInfoDataSource(region).getCurrent().getUv();
+    }
+
+    /** 
+     * Gets the current wind speed.
+     * @param region the region, can be given as US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name
+     * @param unit the unit of speed, send "mph" or "kph" defaults to kilometers per hour if invalid unit given
+     * @return wind speed in either miles per hour or kilometers per hour, depending on unit
+     * @throws JsonProcessingException
+     */
+    public double getWindSpeed(String region, String unit) throws JsonProcessingException {
+        switch(unit) {
+            case "mph":
+                return currentInfoDataSource(region).getCurrent().getWindMph();
+            case "kph":
+                return currentInfoDataSource(region).getCurrent().getWindKph();
+            default:
+                //defaults to kilometers per hour if no valid unit given
+                return currentInfoDataSource(region).getCurrent().getWindKph();
+        }
+    }
+
+    
+    /** 
+     * Checks if it is day at the given region.
+     * @param region the region, can be given as US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name
+     * @return boolean if it is day or not
+     * @throws JsonProcessingException
+     */
+    public boolean checkIsDay(String region) throws JsonProcessingException {
+        return currentInfoDataSource(region).getCurrent().getIsDay() == 1;
     }
 }
