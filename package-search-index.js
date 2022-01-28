41c7ab4db772220ff4ada7f399524c18d13a54e8
Adam Nativ <admlvntv@gmail.com>
2022-01-27 22:40:55 -0500

    Update Javadoc

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.adamcraftmaster"},{"l":"me.adamcraftmaster.schema.currentweather"},{"l":"me.adamcraftmaster.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.adamcraftmaster"},{"l":"me.adamcraftmaster.enums"},{"l":"me.adamcraftmaster.schema.currentweather"},{"l":"me.adamcraftmaster.utils"}];updateSearchResults();
